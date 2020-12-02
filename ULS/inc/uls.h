d849dfe104a948a71783808553eee41446abfbad
Pavlo Marchenko <pmarchenko@e1r8p8.unit.ua>
2020-12-02 15:45:34 +0200

    make

---
+++ ULS/inc/uls.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../libmx/inc/libmx.h"
+
+//======== includes ========//
+#include <fcntl.h>
+#include <limits.h>
+#include <malloc/malloc.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
---
+++ ULS/src/main.c
@@ -0,0 +1,8 @@
+#include "uls.h"
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+ mx_print_unicode_str("Hello world 67");   
+ return 0;
+}
