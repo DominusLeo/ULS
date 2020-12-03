d9ce0a9556c4208cc63bd6d12b3cd5e2c10abca1
Pavlo Marchenko <pmarchenko@e1r9p6.unit.ua>
2020-12-03 17:35:13 +0200

    ff

--- ULS/inc/uls.h
+++ ULS/inc/uls.h
@@ -11,3 +11,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+
+#include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/xattr.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/acl.h>
+
+#include <uuid/uuid.h>
+
+struct dirent
+{
+char d_name[13];
+};
--- ULS/src/main.c
+++ ULS/src/main.c
@@ -1,8 +1,25 @@
 #include "uls.h"
 
-int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
- mx_print_unicode_str("Hello world 67");   
- return 0;
+DIR *opendir(char *dirname);
+
+int main( void )
+{
+    DIR* dirp;
+    struct dirent* direntp;
+
+    dirp = opendir( "/home/fred" );
+    if( dirp == NULL ) {
+        perror( "can't open /home/fred" );
+    } else {
+        for(;;) {
+            direntp = readdir( dirp );
+            if( direntp == NULL ) break;
+
+            printf( "%s\n", direntp->d_name );
+        }
+        
+        closedir( dirp );
+    }
+    
+    return EXIT_SUCCESS;
 }
