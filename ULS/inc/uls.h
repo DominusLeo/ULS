9aca3d064a853d98f4b4c840f53efd34cb72f05c
Pavlo Marchenko <pmarchenko@e1r8p10.unit.ua>
2020-12-04 14:00:04 +0200

    uls

--- ULS/inc/uls.h
+++ ULS/inc/uls.h
@@ -26,7 +26,44 @@
 
 #include <uuid/uuid.h>
 
-struct entry
-{
-char d_name[13];
-};
+# define LS_NORMAL		"\x1B[0m"
+# define LS_RED			"\x1B[31m"
+# define LS_GREEN		"\x1B[32m"
+# define LS_YELLOW		"\x1B[33m"
+# define LS_BLUE		"\x1B[34m"
+# define LS_MAGENTA		"\x1B[35m"
+# define LS_CYAN		"\x1B[36m"
+
+# define LS_NAME_SIZE	255
+# define LS_FLAG_SIZE	10
+# define LS_MODE_SIZE	11
+
+# define R_BIG			0
+# define R_MINI			1
+# define A_MINI			2
+# define L_MINI			3
+# define T_MINI			4
+# define G_BIG			5
+# define V_MINI			6
+# define Y_MINI			7
+# define S_BIG			8
+# define A_BIG			9
+
+typedef struct			s_uls {
+	char				**file_path;
+	struct stat			*file_stat;
+	int					*width;
+	int					i;
+	int					j;
+}						t_uls;
+
+typedef struct			s_lspath {
+	char				flags[LS_FLAG_SIZE + 1];
+	char				**files;
+	char				**dirs;
+	char				**e_path;
+	int					i;
+	int					j;
+}                       t_lspath;
+
+void mx_bzero(void *s, size_t n);
--- ULS/src/main.c
+++ ULS/src/main.c
@@ -1,66 +1,67 @@
 #include "uls.h"
-#include <stdio.h>
 
+void uls_file_count(t_uls *uls, char *flag, char *path) {
+    struct dirent *dir;
+    DIR *directory;
+    int len = 0;
 
-void traverse(char *fn, int indent) {
-  DIR *dir;
-  struct dirent *entry;
-  int count;
-  char path[1025];
-  struct stat info;
-
-  for (count=0; count<indent; count++) printf("  ");
-  printf("%s\n", fn);
-
-  if ((dir = opendir(fn)) == NULL)
-    perror("opendir() error");
-  else {
-    while ((entry = readdir(dir)) != NULL) {
-      if (entry->d_name[0] != '.') {
-        strcpy(path, fn);
-        strcat(path, "/");
-        strcat(path, entry->d_name);
-        if (stat(path, &info) != 0)
-          fprintf(stderr, "stat() error on %s: %s\n", path,
-                  strerror(errno));
-        else if (S_ISDIR(info.st_mode))
-               traverse(path, indent+1);
-      }
+    directory = opendir(path);
+    dir = readdir(directory);
+    while (dir) {
+        if (flag[A_MINI] || (flag[A_BIG] 
+        && mx_strcmp(dir->d_name, ".") 
+        && mx_strcmp(dir->d_name, "..")) || dir->d_name[0] != '.')
+        len++;
+        dir = readdir(directory);
     }
-    closedir(dir);
-  }
+    closedir(directory);
+    uls->file_path = (char **)malloc(sizeof(char *) * len + 1);
+    uls->file_path[len] = NULL;
 }
 
-// void traverse(char *dirname)
-// {
-//   DIR *dir;
-//   struct dirent *ent;
-// //   int count;
-// //   char path[1025];
-// //   struct stat info;
+int uls_readdir(t_uls *uls, char *path, char *flag) {
+    struct dirent *dir;
+    DIR *directory;
+    char *buf = NULL;
 
-//    printf("First pass on '%s':\n",dirname);
-//    if ((dir = opendir(dirname)) == NULL)
-//    {
-//      perror("Unable to open directory");
-//      exit(1);
-//    }
-//    while ((ent = readdir(dir)) != NULL)
-//      printf("%s\n",ent->d_name);
+    directory = opendir(path);
+    dir = readdir(directory);
+    if (!dir) {
+        closedir(directory);
+        return (0);
+    }
+    uls_file_count(uls, flag, path);
+    while (dir) {
+		if (flag[A_MINI] || (flag[A_BIG]
+			&& mx_strcmp(dir->d_name, ".")
+			&& mx_strcmp(dir->d_name, "..")) || dir->d_name[0] != '.')
+			uls->file_path[uls->i++] = mx_strjoin(buf, dir->d_name);
+		dir = readdir(directory);
+	}
+	closedir(directory);
+	free(buf);
+	return (1);
+}
 
-//    printf("Second pass on '%s':\n",dirname);
-//    rewinddir(dir);
-//    while ((ent = readdir(dir)) != NULL)
-//      printf("%s\n",ent->d_name);
-//    if (closedir(dir) != 0)
-//      perror("Unable to close directory");
-// }
+void uls_print_dir(char *path, char *flag) {
+    t_uls uls;
+    uls.file_stat = NULL;
+    uls.width = NULL;
+    if (!(uls_readdir(&uls, path, flag)))
+        return ;
+    // if (flag[V_MINI])
+    //     mx_arrlen(uls.file_path);
+    // if(flag[S_BIG])
 
-int main(int argc,char *argv[])
-{
-  (void)argv;
-  (void)argc;
-  //  traverse("/", 0);
-  traverse(argv[1], 0);
-   exit(0);
+}
+
+int main(int argc, char **argv) {
+    t_lspath pth;
+
+    (void)argv;
+    mx_bzero(pth.flags, LS_FLAG_SIZE + 1); // bzero - заменить на memset
+    if (argc == 1) {
+        uls_print_dir(".", pth.flags);
+    }
+    return 0;
 }
